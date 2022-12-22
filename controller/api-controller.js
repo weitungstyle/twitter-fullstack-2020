08c90870c5fd1da0d8e7e66754b3fb99204278a0
Victor Cheng <weitungstyle@gmail.com>
2022-12-23 00:36:25 +0800

    add api views & controller unable upload photo

--- controller/api-controller.js
+++ controller/api-controller.js
@@ -1,15 +1,14 @@
-const { User, Tweet, Reply, Like, Followship } = require('../models')
-const sequelize = require('sequelize')
+const { User } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const helpers = require('../_helpers')
 
 const apiController = {
   getUserAPI: (req, res, next) => {
-    const loginUserId = helpers.getUser(req).id
-    const editUserId = req.params.id
-    // if (editUserId !== loginUserId) {
-    //   req.flash('error_messages', "You can't edit other's profile!")
-    //   return res.redirect(200, 'back')
-    // }
+    const loginUserId = Number(helpers.getUser(req).id)
+    const editUserId = Number(req.params.id)
+    if (editUserId !== loginUserId) {
+      return res.json({ status: 'error', messages: "You can't edit other's profile!" })
+    }
     return User.findByPk(editUserId, {
       attributes: ['name', 'introduction', 'avatar', 'cover']
     })
@@ -17,11 +16,27 @@ const apiController = {
       .catch(err => next(err))
   },
   postUserAPI: (req, res, next) => {
-    const loginUserId = helpers.getUser(req).id
-    const editUserId = req.params.id
-    // if (editUserId !== loginUserId) throw new Error("You can't edit other's profile!")
-    console.log('complete')
-    res.redirect('back')
+    const loginUserId = Number(helpers.getUser(req).id)
+    const editUserId = Number(req.params.id)
+    const { name, introduction } = req.body
+    const { file } = req
+    if (editUserId !== loginUserId) throw new Error("You can't edit other's profile!")
+    if (!name) throw new Error('User name is required!')
+    return Promise.all([
+      User.findByPk(editUserId),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          name,
+          introduction,
+          avatar: filePath || user.avatar,
+          cover: filePath || user.cover,
+        })
+          .then(() => res.redirect(200, 'back'))
+          .catch(err => next(err))
+      })
   }
 }
 
